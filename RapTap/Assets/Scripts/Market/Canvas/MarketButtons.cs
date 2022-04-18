ee0bf063b92cf7eb034834796a8a9af032c26ae3
cindyoshita <cindynat11@gmail.com>
2022-04-18 01:29:29 -0300

    Implementação da Interface de Compras - ver 1.2
    - Java Doc
    - Melhoria na interface (arrumado o exit button)

--- RapTap/Assets/Scripts/Market/Canvas/MarketButtons.cs
+++ RapTap/Assets/Scripts/Market/Canvas/MarketButtons.cs
@@ -1,12 +1,13 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// Ação ao apertar os Botões presente na Cena
+/// </summary>
 public class MarketButtons : MonoBehaviour
 {
     public void PressButton(GameObject thisImage)
     {
         thisImage.SetActive(true);
-        thisImage.transform.SetAsLastSibling();
+        thisImage.transform.SetAsLastSibling(); // Para 
     }
 }
