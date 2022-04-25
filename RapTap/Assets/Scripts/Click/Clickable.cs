11bf1bee1401d0bc6cece9c49b36e5e0782dba40
edu510 <www.eduardo.f.n@gmail.com>
2022-04-25 20:34:55 -0300

    Arruma o Click Feedback e implementa Animacao do Background

--- RapTap/Assets/Scripts/Click/Clickable.cs
+++ RapTap/Assets/Scripts/Click/Clickable.cs
@@ -8,16 +8,10 @@ public class Clickable : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
 {
     public Text scoreText;
     private int score;
-    public GameObject player;
-    public GameObject crowd;
-    public GameObject musicalNote1;
-    public GameObject musicalNote2;
-    public GameObject enemy;
-    public Rigidbody2D enemyRb;
-    private int playerDisplacement;
-    private int crowdDisplacement;
-    private Vector3 musicSpawnPos = new Vector3(620,590,0);
-    private int enemyHealth = 20;
+
+    public delegate void ClickAction();
+    public static event ClickAction MouseDown;
+    public static event ClickAction MouseUp;
 
     public void Score()
     {
@@ -40,18 +34,9 @@ public class Clickable : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
     {
         // Botão Abaixou
 
-        playerDisplacement = Mathf.RoundToInt(Random.Range(2, 7));
-        crowdDisplacement = Mathf.RoundToInt(Random.Range(3, 6));
-
-        player.transform.position = new Vector2(player.transform.position.x + playerDisplacement, player.transform.position.y + (playerDisplacement-2));
-        player.transform.localScale = player.transform.localScale / 1.02f;
-        crowd.transform.position = new Vector2(crowd.transform.position.x, crowd.transform.position.y + crowdDisplacement);
-        enemy.transform.position = new Vector2(enemy.transform.position.x, enemy.transform.position.y + crowdDisplacement);
-        enemyHealth -= 1;
-        if(enemyHealth <= 0)
+        if(MouseDown != null)
         {
-            enemyRb.AddForceAtPosition(new Vector2(-800f, 2600f), new Vector2(enemy.transform.position.x + 20, enemy.transform.position.y - 30), ForceMode2D.Impulse);
-            enemyHealth = 40;
+            MouseDown();
         }
 
     }
@@ -60,19 +45,10 @@ public class Clickable : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
     {
         // Botão levantou
 
-        player.transform.position = new Vector2(player.transform.position.x - playerDisplacement, player.transform.position.y - (playerDisplacement-2));
-        player.transform.localScale = player.transform.localScale * 1.02f;
-        crowd.transform.position = new Vector2(crowd.transform.position.x, crowd.transform.position.y - crowdDisplacement);
-        enemy.transform.position = new Vector2(enemy.transform.position.x, enemy.transform.position.y - crowdDisplacement);
-        if (Random.Range(0,2) == 1)
+        if (MouseUp != null)
         {
-            Instantiate(musicalNote1, musicSpawnPos, Quaternion.identity);
+            MouseUp();
         }
-        else
-        {
-            Instantiate(musicalNote2, musicSpawnPos, Quaternion.identity);
-        }
-        
-
+     
     }
 }
