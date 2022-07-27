864af7134c6716f4a7b51930b5040d0a106af92e
unknown <tanaka1557@gmail.com>
2022-07-27 19:24:23 -0300

    Revert "Merge pull request #29 from ReiAkio/Edu_branch_03"
    This reverts commit 53698af1e220ea577bf88b2e5ec389dcd7d58411, reversing
    changes made to fb787acc35364317a251988d5600e523a74f621f.

--- RapTap/Assets/Scripts/Lights/StreetLightControl.cs
+++ RapTap/Assets/Scripts/Lights/StreetLightControl.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class StreetLightControl : MonoBehaviour
 {
-    [SerializeField] private GameObject streetLight;
+    public GameObject streetLight;
 
     // Start is called before the first frame update
     void Start()
